cce4ff2f6d21de8c79e180c820755957868e44e3
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2019-03-04 16:02:03 +0100

    bump version

--- clingo-dl.h
+++ clingo-dl.h
@@ -28,11 +28,11 @@
 //! Major version number.
 #define CLINGODL_VERSION_MAJOR 1
 //! Minor version number.
-#define CLINGODL_VERSION_MINOR 0
+#define CLINGODL_VERSION_MINOR 1
 //! Revision number.
 #define CLINGODL_VERSION_REVISION 0
 //! String representation of version.
-#define CLINGODL_VERSION "1.0.0"
+#define CLINGODL_VERSION "1.1.0"
 
 #ifdef __cplusplus
 extern "C" {
