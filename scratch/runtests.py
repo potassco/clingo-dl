c3013cc1008065b638cbabba25cc27e81b6a4de6
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2019-03-17 19:30:56 +0100

    remove some ugliness

--- scratch/runtests.py
+++ scratch/runtests.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 
 from subprocess import call
-from sys import exit
-calls =  [("clingo-dl examples/taskassignment/encoding-dl.lp examples/taskassignment/tai4_4_1.lp -c n=132 0 --outf=3",30)]
+
+calls = [("clingo-dl examples/taskassignment/encoding-dl.lp examples/taskassignment/tai4_4_1.lp -c n=132 0 --outf=3", 30)]
 rc = 0
-for (i, rc_expected) in calls:
+for i, rc_expected in calls:
     rc_call = call(i, shell=True)
-    if (rc_call != rc_expected):
+    if rc_call != rc_expected:
         rc = 1
-        print("Failed: '{}' with error code ".format(i, rc_call))
+        print("Failed: '{}' with error code {}".format(i, rc_call))
 if rc == 0:
-    print ("Success: {} tests passed".format(len(calls)))
+    print("Success: {} tests passed".format(len(calls)))
+
 exit(rc)
