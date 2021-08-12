6fa5c80be3b928ea978653983e13ae46373b3f75
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2021-08-12 15:26:13 +0200

    cleanup + tests

--- app/main.cc
+++ app/main.cc
@@ -24,46 +24,14 @@
 
 #include <clingo.hh>
 #include <clingo-dl.h>
+#include <clingo-dl/app.hh>
 #include <sstream>
 #include <fstream>
-#include <cmath>
-
-using namespace Clingo;
+#include <limits>
 
 using Clingo::Detail::handle_error;
 
-class Rewriter {
-public:
-    Rewriter(clingodl_theory_t *theory, clingo_program_builder_t *builder)
-    : theory_{theory}
-    , builder_{builder} {
-    }
-
-    void rewrite(StringSpan files) {
-        handle_error(clingo_ast_parse_files(files.begin(), files.size(), rewrite_, this, nullptr, nullptr, 0));
-    }
-
-    void rewrite(char const *str) {
-        handle_error(clingo_ast_parse_string(str, rewrite_, this, nullptr, nullptr, 0));
-    }
-
-private:
-    static bool add_(clingo_ast_t *stm, void *data) {
-        auto *self = static_cast<Rewriter*>(data);
-        return clingo_program_builder_add(self->builder_, stm);
-    }
-
-    static bool rewrite_(clingo_ast_t *stm, void *data) {
-        auto *self = static_cast<Rewriter*>(data);
-        return clingodl_rewrite_ast(self->theory_, stm, add_, self);
-    }
-
-    clingodl_theory_t *theory_;
-    clingo_program_builder_t *builder_;
-};
-
-
-class ClingoDLApp : public Clingo::Application, private SolveEventHandler {
+class ClingoDLApp : public Clingo::Application, private Clingo::SolveEventHandler {
 public:
     ClingoDLApp() {
         handle_error(clingodl_create(&theory_));
@@ -71,217 +39,30 @@ public:
     ~ClingoDLApp() { clingodl_destroy(theory_); }
     char const *program_name() const noexcept override { return "clingo-dl"; }
     char const *version() const noexcept override { return CLINGODL_VERSION; }
-    bool on_model(Model &model) override {
+    bool on_model(Clingo::Model &model) override {
         handle_error(clingodl_on_model(theory_, model.to_c()));
         return true;
     }
 
-    int get_bound(Model const &m) {
-        if (!bound_index_) {
-            if (!clingodl_lookup_symbol(theory_, bound_symbol_.to_c(), &bound_index_)) {
-                throw std::runtime_error("variable to minimize not found");
-            }
-        }
-        if (!clingodl_assignment_has_value(theory_, m.thread_id(), bound_index_)) {
-            throw std::runtime_error("variable to minimize is unassigned");
-        }
-        clingodl_value_t value;
-        clingodl_assignment_get_value(theory_, m.thread_id(), bound_index_, &value);
-        // NOTE: minimizinig real values would require an epsilon
-        if (value.type != clingodl_value_type_int) {
-            throw std::runtime_error("only integer minimization is supported");
-        }
-        return value.int_number;
-
-    }
-
-    void add_stats(UserStatistics root) {
-        if (found_bound_) {
-            UserStatistics diff = root.add_subkey("DifferenceLogic", StatisticsType::Map);
-            diff.add_subkey("Optimization", StatisticsType::Value).set_value(opt_.bound_value());
-            diff.add_subkey("Lowerbound", StatisticsType::Value).set_value(opt_.lower_bound());
-        }
-    }
-
-    void on_statistics(UserStatistics step, UserStatistics accu) override {
-        add_stats(step);
-        add_stats(accu);
+    void on_statistics(Clingo::UserStatistics step, Clingo::UserStatistics accu) override {
         handle_error(clingodl_on_statistics(theory_, step.to_c(), accu.to_c()));
     }
 
-    void solve_satisfiability(Control& ctl) {
-        ctl.solve(Clingo::SymbolicLiteralSpan{}, this, false, false).get();
-    }
-
-    class Optimizer {
-    public:
-
-        void set_symbol(Clingo::Symbol opt) {
-            bound_symbol_ = opt;
-        }
-
-        void set_bound(int bound) {
-            has_bound_ = true;
-            bound_value_ = bound;
-        }
-
-        void set_factor(double factor) {
-            factor_ = factor;
-        }
-
-        bool unproven() const {
-            return has_bound_;
-        }
-
-        int bound_value() const {
-            return bound_value_;
-        }
-
-        int lower_bound() const {
-            return lower_bound_;
-        }
-
-        void setup(Control& ctl, clingodl_theory_t* theory) {
-           Clingo::AST::with_builder(ctl, [&](Clingo::AST::ProgramBuilder &builder) {
-               Rewriter rewriter{theory, builder.to_c()};
-               rewriter.rewrite("#program __ub(s,b)."
-                                "#external __ub(b)."
-                                "&diff { s-0 } <= b :- __ub(b)."
-                                "#program __lb(s,b)."
-                                "&diff { 0-s } <= -b-1.");
-            });
-        }
-
-        void prepare_solve(Control& ctl) {
-            release_external(ctl); // release any old externals
-            ubs_ = Number(bound_value_);
-            if (has_bound_) {
-                ctl.ground({{"__ub", {bound_symbol_, ubs_}}});
-                ctl.assign_external(Function("__ub", {ubs_}), TruthValue::True);
-                assigned_ = true;
-            }
-            has_bound_ = false;
-        }
-
-        // Note: it would be possible to add ub(b) as facts after an upper bound has
-        //       been verified. We would get rid of the external. The constraint in
-        //       clingo-dl could be removed nevertheless.
-        void found_model(int bound) {
-            upper_bound_ = bound;
-            double aux = std::max(upper_bound_ - adapt_, lower_bound_+1.0);
-            if (aux > std::numeric_limits<int>::max()) {
-                throw std::overflow_error("Integer overflow during optimization");
-            }
-            bound_value_ = aux;
-            if (bound_value_ < upper_bound_) {
-                has_bound_ = true;
-            }
-            update_adapt();
-        }
-
-        void found_unsat(Control& ctl) {
-            lower_bound_ = bound_value_;
-            adapt_=1;
-            bound_value_ = std::max(static_cast<int>(upper_bound_ - adapt_), lower_bound_);
-            if (lower_bound_<bound_value_) {
-                has_bound_ = true;
-                ctl.ground({{"__lb", {bound_symbol_, Number(lower_bound_)}}});
-            }
-            update_adapt();
-        }
-
-    public:
-
-        void release_external(Control& ctl) const {
-            if (assigned_) {
-                ctl.release_external(Function("__ub", {ubs_}));
-            }
-        }
-
-        void update_adapt() {
-            adapt_ = std::min(static_cast<double>(std::numeric_limits<int>::max()), adapt_*factor_);
-        }
-
-        Clingo::Symbol bound_symbol_;   // which symbol to optimize
-        Clingo::Symbol ubs_;            // last used upper bound symbol
-        int bound_value_ = 0;
-        bool has_bound_ = false;
-        int lower_bound_ = std::numeric_limits<int>::min();
-        int upper_bound_ = std::numeric_limits<int>::max();
-        double factor_ = 1;
-        double adapt_ = 1;
-        bool assigned_ = false;
-    };
-
-    void solve_optimal(Control& ctl) {
-        // NOTE: with an API extension to implement a custom enumerator,
-        //       one could implement this more nicely
-        //       right now this implementation is restricted to the application
-        opt_.setup(ctl, theory_);
-        do
-        {
-            opt_.prepare_solve(ctl);
-            auto h = ctl.solve(Clingo::SymbolicLiteralSpan{}, this, false, true);
-            for (auto &&m : h) {
-                opt_.found_model(get_bound(m));
-                found_bound_ = true;
-                break;
-            }
-            if (h.get().is_unsatisfiable()) {
-                opt_.found_unsat(ctl);
-            }
-            if (h.get().is_interrupted()) {
-                break;
-            }
-        }
-        while (opt_.unproven());
-    }
-
-    void main(Control &ctl, StringSpan files) override {
+    void main(Clingo::Control &ctl, Clingo::StringSpan files) override {
         handle_error(clingodl_register(theory_, ctl.to_c()));
 
-        AST::with_builder(ctl, [&](AST::ProgramBuilder &builder) {
+        Clingo::AST::with_builder(ctl, [&](Clingo::AST::ProgramBuilder &builder) {
             Rewriter rewriter{theory_, builder.to_c()};
             rewriter.rewrite(files);
         });
 
         ctl.ground({{"base", {}}});
-        if (!minimize_) {
-            solve_satisfiability(ctl);
+        if (!opt_cfg_.active) {
+            ctl.solve(Clingo::SymbolicLiteralSpan{}, this, false, false).get();
         }
         else {
-            solve_optimal(ctl);
-            /*
-            // NOTE: with an API extension to implement a custom enumerator,
-            //       one could implement this more nicely
-            //       right now this implementation is restricted to the application
-            AST::with_builder(ctl, [&](Clingo::AST::ProgramBuilder &builder) {
-               Rewriter rewriter{theory_, builder.to_c()};
-               rewriter.rewrite("#program __bound(s,b)."
-                                "&diff { s-0 } <= b."
-                                "#program base.");
-            });
-            do
-            {
-                if (has_bound_) {
-                    ctl.ground({{"__bound", {bound_symbol, Number(bound_value_ - 1)}}});
-                }
-                has_bound_ = false;
-                auto h = ctl.solve(Clingo::SymbolicLiteralSpan{}, this, false, true);
-                for (auto &&m : h) {
-                    bound_value_ = get_bound(m);
-                    has_bound_ = true;
-                    found_bound_ = true;
-                    break;
-                }
-                if (h.get().is_interrupted()) {
-                    break;
-                }
-            }
-            while (has_bound_);
-            */
+            Optimizer{opt_cfg_, *this, theory_}.solve(ctl);
         }
-
     }
 
     bool parse_bound(char const *value) {
@@ -293,11 +74,11 @@ public:
         if (args.empty() || size > 2 || (size > 1 && args[1].type() != Clingo::SymbolType::Number)) {
             return false;
         }
-        minimize_ = true;
-        opt_.set_symbol(args[0]);
-        bound_symbol_ = args[0];
+        opt_cfg_.active = true;
+        opt_cfg_.symbol = args[0];
         if (size > 1) {
-            opt_.set_bound(args[1].number());
+            opt_cfg_.has_initial = true;
+            opt_cfg_.initial = args[1].number();
         }
         return true;
     }
@@ -306,16 +87,16 @@ public:
         std::stringstream strValue;
         strValue.imbue(std::locale::classic());
         strValue << value;
-        double factor;
+        double factor{0};
         strValue >> factor;
-        if (factor < std::numeric_limits<int>::min() || factor > std::numeric_limits<int>::max()) {
+        if (factor < 1 || factor > std::numeric_limits<int>::max()) {
             throw std::overflow_error("minimize-factor out of bounds");
         }
-        opt_.set_factor(factor);
+        opt_cfg_.factor = factor;
         return strValue.rdbuf()->in_avail() == 0;
     }
 
-    void register_options(ClingoOptions &options) override {
+    void register_options(Clingo::ClingoOptions &options) override {
         handle_error(clingodl_register_options(theory_, options.to_c()));
         char const * group = "Clingo.DL Options";
         options.add(group, "minimize-variable",
@@ -325,10 +106,9 @@ public:
             "      <initial> : upper bound for the variable",
             [this](char const *value) { return parse_bound(value); });
         options.add(group, "minimize-factor",
-            "Decrease optimization steps \n"
-            "      <factor>   : {multiplication factor, 1=linear (default)}",
+            "Factor to adjust optimization \n"
+            "      <factor>   : {multiplication factor, 1=linear (default)}\n",
             [this](char const *value) { return parse_factor(value); });
-
     }
 
     void validate_options() override {
@@ -337,11 +117,7 @@ public:
 
 private:
     clingodl_theory_t *theory_;
-    Optimizer opt_;
-    Clingo::Symbol bound_symbol_;
-    size_t bound_index_ = 0;
-    bool minimize_ = false;
-    bool found_bound_ = false;
+    OptimizerConfig opt_cfg_;
 };
 
 int main(int argc, char *argv[]) {
---
+++ libclingo-dl/tests/optimize.cc
@@ -0,0 +1,84 @@
+#include <clingo.hh>
+#include <clingo-dl.h>
+#include <clingo-dl/propagator.hh>
+#include <clingo-dl/app.hh>
+#include "catch.hpp"
+
+namespace {
+
+struct SolveHandler : public Clingo::SolveEventHandler {
+public:
+    SolveHandler(clingodl_theory_t *theory)
+    : theory_{theory} {
+    }
+    Clingo::SymbolVector const &symbols() const {
+        return symbols_;
+    }
+private:
+    bool on_model(Clingo::Model &model) override {
+        REQUIRE(clingodl_on_model(theory_, model.to_c()));
+        symbols_ = model.symbols(Clingo::ShowType::Theory);
+        std::sort(symbols_.begin(), symbols_.end());
+        return false;
+    }
+    void on_statistics(Clingo::UserStatistics step, Clingo::UserStatistics accu) override {
+        REQUIRE(clingodl_on_statistics(theory_, step.to_c(), accu.to_c()));
+    }
+    clingodl_theory_t *theory_;
+    Clingo::SymbolVector symbols_;
+};
+
+void parse_program(clingodl_theory_t *theory, Clingo::Control &ctl, const char *str) {
+    Clingo::AST::with_builder(ctl, [&](Clingo::AST::ProgramBuilder &builder) {
+        Rewriter rewriter{theory, builder.to_c()};
+        rewriter.rewrite(str);
+    });
+}
+
+Clingo::Symbol assign(Clingo::Symbol name, int value) {
+    return Clingo::Function("dl", {name, Clingo::Number(value)});
+}
+
+Clingo::SymbolVector optimize(Clingo::Control &ctl, Clingo::Symbol bound, double factor, char const *prg) {
+    clingodl_theory_t *theory{nullptr};
+    REQUIRE(clingodl_create(&theory));
+    REQUIRE(clingodl_register(theory, ctl.to_c()));
+    parse_program(theory, ctl, prg);
+    ctl.ground({{"base", {}}});
+    REQUIRE(clingodl_prepare(theory, ctl.to_c()));
+    SolveHandler handler = {theory};
+    OptimizerConfig cfg;
+    cfg.symbol = bound;
+    cfg.factor = factor;
+    Optimizer{cfg, handler, theory}.solve(ctl);
+    REQUIRE(clingodl_destroy(theory));
+    return handler.symbols();
+}
+
+} // namespace
+
+TEST_CASE("optimize", "[clingo-dl]") {
+    auto a = Clingo::Id("a"),  b = Clingo::Id("b");
+    SECTION("sat") {
+        for (auto factor : {1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 64.0, 128.0, 256.0, 512.0, 1024.0}) {
+            Clingo::Control ctl{{"1"}};
+            REQUIRE(optimize(
+                ctl, b, factor,
+                "&diff { a - 0 } >=  100.\n"
+                "&diff { b - 0 } >= -100.\n"
+                "&diff { a - b } <=  150.\n") == Clingo::SymbolVector{assign(a, 100), assign(b, -50)});
+            REQUIRE(ctl.statistics()["user_step"]["DifferenceLogic"].has_subkey("Optimization"));
+            REQUIRE(ctl.statistics()["user_step"]["DifferenceLogic"]["Optimization"].value() == -50);
+        }
+    }
+    SECTION("unsat") {
+        Clingo::Control ctl{{"1"}};
+        REQUIRE(optimize(
+            ctl, b, 1.0,
+            "&diff { a - 0 } >=  100.\n"
+            "&diff { b - 0 } >= -100.\n"
+            "&diff { a - b } <=  150.\n"
+            "&diff { b - 0 } <  -50.\n") == Clingo::SymbolVector{});
+        REQUIRE(!ctl.statistics()["user_step"]["DifferenceLogic"].has_subkey("Optimization"));
+    }
+}
