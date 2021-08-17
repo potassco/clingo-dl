1a1379314a231700118699cdd325a0ba03c6b036
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2021-08-17 08:41:39 +0200

    more refactoring

--- libclingo-dl/clingo-dl/graph.hh
+++ libclingo-dl/clingo-dl/graph.hh
@@ -176,147 +176,12 @@ public:
     [[nodiscard]] bool valid_node(int idx) const;
     [[nodiscard]] int node_value_defined(int idx) const;
     [[nodiscard]] bool has_value(int idx) const;
-
     [[nodiscard]] T node_value(int idx) const;
-
     [[nodiscard]] bool edge_is_active(int edge_idx) const;
-
     [[nodiscard]] bool can_propagate() const;
     void disable_propagate();
     void ensure_decision_level(int level, bool enable_propagate);
-
-    template <class F>
-    [[nodiscard]] bool add_edge(int uv_idx, F f) {
-#ifdef CROSSCHECK
-        for (auto &node : nodes_) {
-            assert(!node.visited_from);
-        }
-#endif
-        assert(visited_from_.empty());
-        assert(costs_heap_.empty());
-        int level = current_decision_level_();
-        auto &uv = edges_[uv_idx];
-        // NOTE: would be more efficient if relevant would return statically false here
-        //       for the compiler to make comparison cheaper
-        auto &m = *static_cast<HFM *>(this);
-
-        // initialize the nodes of the edge to add
-        auto &u = nodes_[uv.from];
-        auto &v = nodes_[uv.to];
-        if (!u.defined()) {
-            set_potential(u, level, 0);
-        }
-        if (!v.defined()) {
-            set_potential(v, level, 0);
-        }
-        v.cost_from = u.potential() + uv.weight - v.potential();
-        ++stats_.edges_added;
-        if (v.cost_from < 0) {
-            costs_heap_.push(m, uv.to);
-            visited_from_.emplace_back(uv.to);
-            v.visited_from = 1;
-            v.path_from = uv_idx;
-        }
-        else {
-            ++stats_.edges_skipped;
-        }
-
-        int dfs = 0;
-        // detect negative cycles
-        while (!costs_heap_.empty() && !u.visited_from) {
-            auto s_idx = costs_heap_.pop(m);
-            auto &s = nodes_[s_idx];
-            assert(s.visited_from);
-            s.visited_from = ++dfs;
-            set_potential(s, level, s.potential() + s.cost_from);
-            for (auto st_idx : s.outgoing) {
-                ++stats_.propagate_cost_add;
-                assert(st_idx < numeric_cast<int>(edges_.size()));
-                auto &st = edges_[st_idx];
-                auto &t = nodes_[st.to];
-                auto c = s.potential() + st.weight - t.potential();
-                if (c < (t.visited_from ? t.cost_from : 0)) {
-                    assert(c < 0);
-                    t.path_from = st_idx;
-                    t.cost_from = c;
-                    if (!t.visited_from) {
-                        t.visited_from = 1;
-                        visited_from_.emplace_back(st.to);
-                        costs_heap_.push(m, st.to);
-                    }
-                    else {
-                        costs_heap_.decrease(m, m.offset(st.to));
-                    }
-                }
-            }
-        }
-
-        bool consistent = true;
-        if (!u.visited_from) {
-            // add the edge to the graph
-            u.outgoing.emplace_back(uv_idx);
-            v.incoming.emplace_back(uv_idx);
-            changed_edges_.emplace_back(uv_idx);
-#ifdef CROSSCHECK
-            // NOTE: just a check that will throw if there is a cycle
-            bellman_ford(changed_edges_, uv.from);
-#endif
-        }
-        else {
-            // gather the edges in the negative cycle
-            neg_cycle_.clear();
-            neg_cycle_.push_back(v.path_from);
-            auto next_idx = edges_[v.path_from].from;
-            while (uv.to != next_idx) {
-                auto &next = nodes_[next_idx];
-                neg_cycle_.push_back(next.path_from);
-                next_idx = edges_[next.path_from].from;
-            }
-#ifdef CROSSCHECK
-            T weight = 0;
-            for (auto &edge_idx : neg_cycle_) {
-                weight += edges_[edge_idx].weight;
-            }
-            assert(weight < 0);
-#endif
-            consistent = f(neg_cycle_);
-        }
-
-        if (propagate_ >= PropagationMode::Trivial && consistent) {
-            if (visited_from_.empty() || propagate_ == PropagationMode::Trivial) {
-                consistent = with_incoming(uv.from, f, [&](int t_idx, int ts_idx) {
-                    auto &ts = edges_[ts_idx];
-                    if (t_idx == uv.to && uv.weight + ts.weight < 0) {
-                        neg_cycle_.emplace_back(uv_idx);
-                        neg_cycle_.emplace_back(ts_idx);
-                        ++stats_.false_edges_trivial;
-                        return true;
-                    }
-                    return false;
-                });
-            }
-            else if (propagate_ >= PropagationMode::Weak) {
-                consistent = cheap_propagate(uv.from, uv.from, f);
-                if (propagate_ >= PropagationMode::WeakPlus && consistent) {
-                    for (auto &s_idx : visited_from_) {
-                        if (!cheap_propagate(uv.from, s_idx, f)) {
-                            consistent = false;
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-        // reset visited flags
-        for (auto &x : visited_from_) {
-            nodes_[x].visited_from = 0;
-        }
-        visited_from_.clear();
-        costs_heap_.clear();
-
-        return consistent;
-    }
-
+    [[nodiscard]] bool add_edge(int uv_idx, std::function<bool(std::vector<int>)> f);
     [[nodiscard]] bool propagate(int xy_idx, Clingo::PropagateControl &ctl);
     void backtrack();
     void remove_candidate_edge(int uv_idx);
@@ -324,73 +189,10 @@ public:
 
 private:
     template <class P, class F>
-    [[nodiscard]] bool with_incoming(int s_idx, P p, F f) {
-        auto &s = nodes_[s_idx];
-        auto &in = s.candidate_incoming;
-        auto jt = in.begin();
-        for (auto it = jt, ie = in.end(); it != ie; ++it) {
-            auto &ts_idx = *it;
-            auto &ts = edges_[ts_idx];
-            auto t_idx = ts.from;
-            auto &t = nodes_[t_idx];
-            if (!edge_states_[ts_idx].active) {
-                edge_states_[ts_idx].removed_incoming = true;
-                continue;
-            }
-            neg_cycle_.clear();
-            if (f(t_idx, ts_idx)) {
-                edge_states_[ts_idx].removed_incoming = true;
-                remove_candidate_edge(ts_idx);
-                if (!p(neg_cycle_)) {
-                    in.erase(jt, it+1);
-                    return false;
-                }
-                continue;
-            }
-            *jt++ = *it;
-        }
-        in.erase(jt, in.end());
-        return true;
-    }
+    [[nodiscard]] bool with_incoming(int s_idx, P p, F f);
 
     template <class F>
-    [[nodiscard]] bool cheap_propagate(int u_idx, int s_idx, F f) {
-        // we check for the following case:
-        // u ->* s -> * t
-        //       ^-----/
-        //          ts
-        return with_incoming(s_idx, f, [&](int t_idx, int ts_idx) {
-            auto &s = nodes_[s_idx];
-            auto &t = nodes_[t_idx];
-            auto &ts = edges_[ts_idx];
-            if (s.visited_from < t.visited_from) {
-                T weight = t.potential() - s.potential();
-                if (weight + ts.weight < 0) {
-                    T check = 0;
-                    auto r_idx = t_idx;
-                    while (u_idx != r_idx && s_idx != r_idx) {
-                        auto &r = nodes_[r_idx];
-                        auto &rr = edges_[r.path_from];
-                        neg_cycle_.emplace_back(r.path_from);
-                        r_idx = rr.from;
-                        check += rr.weight;
-                    }
-                    if (r_idx == s_idx) {
-                        if (u_idx == s_idx) {
-                            ++stats_.false_edges_weak;
-                        }
-                        else {
-                            ++stats_.false_edges_weak_plus;
-                        }
-                        assert(weight == check);
-                        neg_cycle_.emplace_back(ts_idx);
-                        return true;
-                    }
-                }
-            }
-            return false;
-        });
-    }
+    [[nodiscard]] bool cheap_propagate(int u_idx, int s_idx, F f);
 
     void add_candidate_edge(int uv_idx);
     [[nodiscard]] bool propagate_edge_true(int uv_idx, int xy_idx);
