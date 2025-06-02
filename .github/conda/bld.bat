f0d47d83bbc645a2c54af893164520eac556ebd2
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2025-06-02 10:33:57 +0200

    get conda releases running

---
+++ .github/conda/bld.bat
@@ -0,0 +1,12 @@
+cmake -S . -B build -G "Visual Studio 17 2022" ^
+    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+    -DCMAKE_INSTALL_LIBDIR="lib" ^
+    -DPython_ROOT_DIR="%PREFIX%" ^
+    -DPython_EXECUTABLE="%PYTHON%" ^
+    -DCLINGODL_MANAGE_RPATH=OFF ^
+    -DCLINGODL_BUILD_TESTS=ON ^
+    -DPYCLINGODL_INSTALL_DIR="%SP_DIR%"
+
+cmake --build build --config Release
+ctest --test-dir build -C Release
+cmake --build build --target install --config Release
